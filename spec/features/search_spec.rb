786f0d09a3ea459482fa7fbbe63028f045aa7994
Dunya Kirkali <dunyakirkali@gmail.com>
2014-07-01 23:44:53 +0200

    search feature PART II

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -9,6 +9,12 @@ describe 'Search Feature' do
       login_as(user)
     end
     
-    it 'I should be find the shortest walking path between two points'
+    it 'I should be find the shortest walking path between two points' do
+      fill_in 'start_point', with: 'Buijs Ballotstraat 106, Den Haag'
+      fill_in 'end_point', with: 'Fahreheitstraat 102, Den Haag'
+      click_on 'search'
+      
+      expect(page).to have_content('Route')
+    end
   end
 end
