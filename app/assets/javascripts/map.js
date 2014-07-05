ded3db5bcd4636a4b61a6f485c6c24612dbbc77f
Dunya Kirkali <dunyakirkali@gmail.com>
2014-07-05 23:57:29 +0200

    hmmm

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,4 +1,34 @@
 $( document ).ready(function() {
+	var directionsDisplay;
+	var directionsService = new google.maps.DirectionsService();
+	var map;
 
-		
+	function initialize() {
+	  directionsDisplay = new google.maps.DirectionsRenderer();
+	  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+	  var mapOptions = {
+	    zoom:7,
+	    center: chicago
+	  }
+	  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	  directionsDisplay.setMap(map);
+	}
+
+	function calcRoute() {
+		var $map = $('#map');
+	  var start = new google.maps.LatLng($map.data('start-lat'), $map.data('start-long'));
+	  var end = new google.maps.LatLng($map.data('end-lat'), $map.data('end-long'));
+	  var request = {
+	    origin:start,
+	    destination:end,
+	    travelMode: google.maps.TravelMode.WALKING
+	  };
+	  directionsService.route(request, function(result, status) {
+	    if (status == google.maps.DirectionsStatus.OK) {
+	      directionsDisplay.setDirections(result);
+	    }
+	  });
+	}
+  initialize();
+	calcRoute();
 });
