e4c7ee4ebf7690275f36059e0f1b155edf31f4b6
Dunya Kirkali <dunyakirkali@gmail.com>
2014-07-06 14:19:27 +0200

    rute calculation

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,23 +1,22 @@
 $( document ).ready(function() {
-	var directionsDisplay;
+	var directionsDisplay = new google.maps.DirectionsRenderer();
 	var directionsService = new google.maps.DirectionsService();
 	var map;
-
+	var $map = $('#map');
+  var start = new google.maps.LatLng($map.data('start-lat'), $map.data('start-long'));
+  var end = new google.maps.LatLng($map.data('end-lat'), $map.data('end-long'));
+	var path;
+	
 	function initialize() {
-	  directionsDisplay = new google.maps.DirectionsRenderer();
-	  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
 	  var mapOptions = {
-	    zoom:7,
-	    center: chicago
+	    zoom:7
 	  }
 	  map = new google.maps.Map(document.getElementById("map"), mapOptions);
 	  directionsDisplay.setMap(map);
+		service = new google.maps.places.PlacesService(map);
 	}
 
-	function calcRoute() {
-		var $map = $('#map');
-	  var start = new google.maps.LatLng($map.data('start-lat'), $map.data('start-long'));
-	  var end = new google.maps.LatLng($map.data('end-lat'), $map.data('end-long'));
+	function findPath() {
 	  var request = {
 	    origin:start,
 	    destination:end,
@@ -26,9 +25,53 @@ $( document ).ready(function() {
 	  directionsService.route(request, function(result, status) {
 	    if (status == google.maps.DirectionsStatus.OK) {
 	      directionsDisplay.setDirections(result);
+				path = result.routes[0];
+				console.log(path)
+				searchForBarsAtPath();
 	    }
 	  });
 	}
+	
+	function searchForBarsAtPath() {
+		$.each(path.overview_path, function( index, point ) {
+			searchForBarsAtPoint(point);
+		});
+	}
+	
+	function searchForBarsAtPoint(point) {
+		var request = {
+	    location: point,
+	    radius: '1',
+	    query: 'bar'
+	  };
+		service.textSearch(request, callback);
+	}
+		
+	function callback(results, status) {
+	  if (status == google.maps.places.PlacesServiceStatus.OK) {
+	    for (var i = 0; i < results.length; i++) {
+	      var place = results[i];
+	      createMarker(results[i]);
+	    }
+	  }
+	}
+	
+	function createMarker(place) {
+		var bars = $('.bars ul');
+		console.log(place)
+		var marker = new google.maps.Marker({
+      position: place.geometry.location,
+      map: map,
+			title: place.name
+	  });
+		bars.append(
+      $('<li>').append(
+        $('<a>').attr('href','#').append(
+          $('<span>').attr('class', 'tab').append(place.name)
+    )));
+	}
+		
+// Main
   initialize();
-	calcRoute();
+	findPath();
 });
